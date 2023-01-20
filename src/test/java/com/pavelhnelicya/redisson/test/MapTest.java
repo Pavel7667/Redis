843a32eac62e12087bbd38bfa7e61acf9faff706
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-20 23:27:31 +0200

    push Map with notOnlyString <String,String> into Redis

--- src/test/java/com/pavelhnelicya/redisson/test/MapTest.java
+++ src/test/java/com/pavelhnelicya/redisson/test/MapTest.java
@@ -1,11 +1,15 @@
 package com.pavelhnelicya.redisson.test;
 
+import com.pavelhnelicya.redisson.test.dto.Student;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RMapReactive;
 import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class MapTest extends BaseTest {
@@ -35,5 +39,19 @@ public class MapTest extends BaseTest {
 
         StepVerifier.create(mapReactive.putAll(mapJava).then())
                 .verifyComplete();
+    }
+
+    @Test
+    public void mapNotOnlyStringTest() {
+        //Map<Integer,Student>
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(Integer.class, Student.class);
+        RMapReactive<Integer, Student> mapReactive = this.client.getMap("users", codec);
+
+        Student student1 = new Student("sam", 10, "atlanta", List.of(1,2,3));
+        Student student2 = new Student("jake", 14, "miami", List.of(11,22,33));
+        Mono<Student> mono1 = mapReactive.put(1,student1);
+        Mono<Student> mono2 = mapReactive.put(2,student2);
+
+        StepVerifier.create(mono1.concatWith(mono2)).verifyComplete();
     }
 }
