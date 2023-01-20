828d88ef5ca83d8d8462610f057d328b1d4a0e05
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-20 16:01:30 +0200

    Create connection with Server BeforeAll all Tests And shutdown connection after Tests

---
+++ src/test/java/com/pavelhnelicya/redisson/test/BaseTest.java
@@ -0,0 +1,29 @@
+package com.pavelhnelicya.redisson.test;
+
+import com.pavelhnelicya.redisson.test.config.RedissonConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.redisson.api.RedissonReactiveClient;
+
+/**
+ * Create connection with Server BeforeAll all Tests
+ * And shutdown connection after Tests
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class BaseTest {
+
+    private final RedissonConfig redissonConfig = new RedissonConfig();
+
+    protected RedissonReactiveClient client;
+
+    @BeforeAll
+    public void setClient() {
+        this.client = this.redissonConfig.getReactiveClient();
+    }
+
+    @AfterAll
+    public void shutdown() {
+        this.client.shutdown();
+    }
+}
