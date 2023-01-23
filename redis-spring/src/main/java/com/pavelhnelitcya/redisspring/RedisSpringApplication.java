e76fc212cfa251f4c55c9c805af6fa71788adc5c
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-23 17:12:02 +0200

    Simple API that counting Fibonacci number in URL request, with using Radisson Caching technique

---
+++ redis-spring/src/main/java/com/pavelhnelitcya/redisspring/RedisSpringApplication.java
@@ -0,0 +1,14 @@
+package com.pavelhnelitcya.redisspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class RedisSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringApplication.class, args);
+	}
+}
